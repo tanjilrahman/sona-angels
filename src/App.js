250e1695c6badc549450b05cbe18ee3268a6868e
Tanjil Rahman <tanjil.rahman01@yahoo.com>
2021-09-21 14:03:15 +0600

    Updated meta data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { Helmet } from 'react-helmet';
 import './index.css';
 import Header from './components/Header';
 import Hero from './components/Hero';
@@ -8,50 +7,6 @@ import Footer from './components/Footer';
 function App() {
   return (
     <div>
-      <Helmet>
-        <title>
-          Sona Angels, Join an elite group Full of talented and impressive South
-          Asian woman leaders in New Jersey
-        </title>
-        <meta
-          name='description'
-          content='A family owned business in the heart of New Jersey aiming to empower women with the finest pieces of Indian jewelry.'
-        />
-
-        <meta
-          itemprop='name'
-          content='Sona Angels, Join an elite group Full of talented and impressive South Asian woman leaders in New Jersey'
-        />
-        <meta
-          itemprop='description'
-          content='A family owned business in the heart of New Jersey aiming to empower women with the finest pieces of Indian jewelry.'
-        />
-        <meta itemprop='image' content='/sonalogofinalnew_400x.webp' />
-
-        <meta property='og:url' content='https://www.sonajewelers.com' />
-        <meta property='og:type' content='website' />
-        <meta
-          property='og:title'
-          content='Sona Angels, Join an elite group Full of talented and impressive South Asian woman leaders in New Jersey'
-        />
-        <meta
-          property='og:description'
-          content='A family owned business in the heart of New Jersey aiming to empower women with the finest pieces of Indian jewelry.'
-        />
-        <meta property='og:image' content='/sonalogofinalnew_400x.webp' />
-
-        <meta name='twitter:card' content='summary_large_image' />
-        <meta
-          name='twitter:title'
-          content='Sona Angels, Join an elite group Full of talented and impressive South Asian woman leaders in New Jersey'
-        />
-        <meta
-          name='twitter:description'
-          content='A family owned business in the heart of New Jersey aiming to empower women with the finest pieces of Indian jewelry.'
-        />
-        <meta name='twitter:image' content='/sonalogofinalnew_400x.webp' />
-      </Helmet>
-
       <Header />
       <div className='md:max-w-screen-sm lg:max-w-screen-xl mx-auto px-4 xl:px-0 overflow-x-hidden'>
         <Hero />
