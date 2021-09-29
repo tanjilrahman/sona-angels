015711060c4e8cac67a86b54116037369ba11a20
Tanjil Rahman <tanjil.rahman01@yahoo.com>
2021-09-29 20:23:14 +0600

    alternate success page

--- src/App.js
+++ src/App.js
@@ -1,29 +1,18 @@
 import './index.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Body from './components/Body';
 import Footer from './components/Footer';
-import Success from './components/Success';
 
 function App() {
   return (
     <div>
-      <Router>
-        <Switch>
-          <Route path='/success' component={Success} />
-          <Route path='/'>
-            <div>
-              <Header />
-              <div className='md:max-w-screen-sm lg:max-w-screen-xl mx-auto px-4 xl:px-0 overflow-x-hidden'>
-                <Hero />
-                <Body />
-              </div>
-              <Footer />
-            </div>
-          </Route>
-        </Switch>
-      </Router>
+      <Header />
+      <div className='md:max-w-screen-sm lg:max-w-screen-xl mx-auto px-4 xl:px-0 overflow-x-hidden'>
+        <Hero />
+        <Body />
+      </div>
+      <Footer />
     </div>
   );
 }
